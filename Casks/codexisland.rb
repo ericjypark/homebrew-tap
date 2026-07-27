d08e9c76e58020777b967a200849efbf784969ad
ericjypark <2019147551@yonsei.ac.kr>
2026-07-27 12:50:58 +0000

    chore: bump codexisland to 0.1.21

--- Casks/codexisland.rb
+++ Casks/codexisland.rb
@@ -1,6 +1,6 @@
 cask "codexisland" do
-  version "0.1.20"
-  sha256 "48fdde701929755dc510f97c6d6eddc24e515c2f2606e7ca45f7ae5447cf0b69"
+  version "0.1.21"
+  sha256 "8e58fbdbf3b14f82dc015a22648aa62dd44eb7a19568d24e40f6882683f5e537"
 
   url "https://github.com/ericjypark/codex-island/releases/download/v#{version}/CodexIsland-#{version}.dmg"
   name "CodexIsland"
